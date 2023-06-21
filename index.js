0343d10492bba2f3eb11634eee6395b267527877
mertee05 <131835458+mertee05@users.noreply.github.com>
2023-06-21 11:44:27 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+const inputBtn = document.getElementById("input-btn");
+const inputEl = document.getElementById("input-el");
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  console.log(myLeads);
+});
+
+for (let i = 0; i < myLeads.length; i++) {
+  console.log(myLeads[i]);
+}
