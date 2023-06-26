8423525aa88f2f27c33b540e704481af62ae72b4
mertee05 <131835458+mertee05@users.noreply.github.com>
2023-06-26 11:48:09 -0700

    more CSS functionality

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+let myLeads = [];
 const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
-  console.log(myLeads);
+  renderLeads();
+  inputEl.value = "";
 });
 
-for (let i = 0; i < myLeads.length; i++) {
-  ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems +=
+      "<li><a target='_blank' href='" +
+      myLeads[i] +
+      "'>" +
+      myLeads[i] +
+      "</a></li>";
+  }
+  ulEl.innerHTML = listItems;
 }
