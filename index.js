028bd0f28c74009b9617591822302ec7e065f54b
mertee05 <131835458+mertee05@users.noreply.github.com>
2023-07-06 12:58:25 -0700

    passing arguments

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 let myLeads = [];
-let oldLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
